fd0a309163de37a7cc5199bdbe2acf5e6fc54d26
Arni Magnusson <thisisarni@gmail.com>
2025-03-24 11:23:55 +1100

    Add get_catch() function

---
+++ R/get_catch.R
@@ -0,0 +1,4 @@
+get_catch <- function(file)
+{
+  catch <- read.taf(file)
+}
--- _targets.R
+++ _targets.R
@@ -7,7 +7,13 @@ tar_source()
 
 list(
   tar_target(
+    name = catch_file,
+    command = "data/initial/catch.csv",
+    format = "file",
+    description = "Annual catch prepared by WGEF in 2015, containing data from 2012-2014"
+  ),
+  tar_target(
     name = catch,
-    command = read.taf("data/catch.csv")
+    command = get_catch(catch_file)
   )
 )
