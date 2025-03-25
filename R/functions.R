5e4b3a904790e1ebb966719fd63c7463360b106e
Arni Magnusson <thisisarni@gmail.com>
2025-03-25 16:00:32 +1100

    Visual space

--- R/functions.R
+++ R/functions.R
@@ -15,7 +15,7 @@ data_survey <- function(file)
   survey <- survey[survey$Year %in% 1993:2014, names(survey) != "Unknown"]
 
   # Scale each survey to average 1, combine index as average of three surveys
-  survey[-1] <- sapply(survey[-1], function(x) x/mean(x, na.rm=TRUE))
+  survey[-1] <- sapply(survey[-1], function(x) x / mean(x, na.rm=TRUE))
   survey$Index <- rowMeans(survey[-1])
 
   # Finalize table
